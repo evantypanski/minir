f64540da7ecad4e5d6bd50022e561546da7a6378
Evan Typanski <evantypanski@gmail.com>
2024-05-15 06:49:49 -0400

    Refactor some output test stuff

--- src/test/output.zig
+++ src/test/output.zig
@@ -6,52 +6,24 @@ const std = @import("std");
 const Driver = @import("../driver/Driver.zig");
 const CommandLine = @import("../driver/command_line.zig").CommandLine;
 
+var outDir: std.testing.TmpDir = undefined;
+
 // Runs the given test, where test is within the top-level 'tests/output' dir
 fn run(comptime name: []const u8) !void {
     const tests_dir = try std.fs.cwd().openDir("tests/output", .{});
     const test_file = try tests_dir.realpathAlloc(std.testing.allocator, name);
     defer std.testing.allocator.free(test_file);
 
-    var outDir = std.testing.tmpDir(.{});
-    defer outDir.cleanup();
-
     // Bytecode interpreter
-    const byteOut = try outDir.dir.createFile("byte.out", .{ .read = true });
-
-    const byteInterpret = CommandLine.InterpretConfig {
-        .filename = test_file,
-        .interpreter_type = .byte
-    };
-    const byteConfig = CommandLine.CommandLineResult {
-        .interpret = byteInterpret
-    };
-
-    try Driver.init(std.testing.allocator, byteOut.writer())
-            .drive_with_opts(byteConfig);
-    try byteOut.seekTo(0);
-    const byteResult = try byteOut.readToEndAlloc(std.testing.allocator, 1000);
-    defer std.testing.allocator.free(byteResult);
+    const byte_result = try get_output(test_file, .byte);
+    defer std.testing.allocator.free(byte_result);
 
     // Treewalk interpreter
-    const treewalkOut =
-            try outDir.dir.createFile("treewalk.out", .{ .read = true });
-    const treewalkInterpret = CommandLine.InterpretConfig {
-        .filename = test_file,
-        .interpreter_type = .treewalk
-    };
-    const treeConfig = CommandLine.CommandLineResult {
-        .interpret = treewalkInterpret
-    };
-
-    try Driver.init(std.testing.allocator, treewalkOut.writer())
-            .drive_with_opts(treeConfig);
-    try treewalkOut.seekTo(0);
-    const treewalkResult =
-            try treewalkOut.readToEndAlloc(std.testing.allocator, 1000);
-    defer std.testing.allocator.free(treewalkResult);
+    const treewalk_result = try get_output(test_file, .treewalk);
+    defer std.testing.allocator.free(treewalk_result);
 
     // Ensure the bytecode and treewalk interpreter are the same
-    try std.testing.expectEqualStrings(byteResult, treewalkResult);
+    try std.testing.expectEqualStrings(byte_result, treewalk_result);
 
     // If we have <filename>.expected, then check against that too
     const expected_name = name ++ ".expected";
@@ -59,10 +31,28 @@ fn run(comptime name: []const u8) !void {
     const expected = try expected_file.readToEndAlloc(std.testing.allocator, 1000);
     defer std.testing.allocator.free(expected);
 
-    try std.testing.expectEqualStrings(expected, byteResult);
+    try std.testing.expectEqualStrings(expected, byte_result);
+}
+
+fn get_output(test_file: []const u8, comptime interpreter: enum { byte, treewalk }) ![]u8 {
+    const out = try outDir.dir.createFile(@tagName(interpreter) ++ ".out", .{ .read = true });
+
+    const config = CommandLine.InterpretConfig {
+        .filename = test_file,
+        .interpreter_type = .byte
+    };
+    const cmd = CommandLine.CommandLineResult {
+        .interpret = config,
+    };
+
+    try Driver.init(std.testing.allocator, out.writer()).drive_with_opts(cmd);
+    try out.seekTo(0);
+    return try out.readToEndAlloc(std.testing.allocator, 1000);
 }
 
 test "output examples" {
+    outDir = std.testing.tmpDir(.{});
+
     try run("minimal.min");
     try run("print.min");
     try run("binops.min");
@@ -70,4 +60,6 @@ test "output examples" {
     try run("count_to_42.min");
     try run("recursive_fib.min");
     try run("unary_neg.min");
+
+    outDir.cleanup();
 }
